4fcc917a5f00ba935a2dd45a4e58ba50a53a7d52
Sebastian Poblete <pseba9006@gmail.com>
2025-11-07 12:50:46 -0300

    feat(gemini.client): integrada api de gemini

--- src/config.ts
+++ src/config.ts
@@ -6,11 +6,20 @@ export const config = {
   port: process.env.PORT || 8000,
 
   // Tokens y Endpoints
+  // Meta y API Local
   metaVerifyToken: process.env.META_VERIFY_TOKEN,
   localApiEndpoint: process.env.LOCAL_API_ENDPOINT,
   metaAccessToken: process.env.ACCESS_TOKEN,
   metaPhoneNumberId: process.env.PHONE_NUMBER_ID,
+
+  // (MODIFICADO) Vertex AI / GCP
+  // (Borramos 'geminiApiKey')
   geminiApiKey: process.env.GEMINI_API_KEY,
+  gcpProjectId: process.env.GCP_PROJECT_ID,
+  gcpLocation: process.env.GCP_LOCATION,
+  gcpDataStoreId: process.env.DATA_STORE_ID,
+
+  // Supabase
   supabaseUrl: process.env.SUPABASE_URL,
   supabaseServiceKey: process.env.SUPABASE_SERVICE_KEY,
 };
@@ -28,12 +37,18 @@ if (!config.metaAccessToken) {
 if (!config.metaPhoneNumberId) {
   throw new Error("PHONE_NUMBER_ID no está definido en .env");
 }
-if (!config.geminiApiKey) {
-  throw new Error("GEMINI_API_KEY no está definido en .env");
-}
+
 if (!config.supabaseUrl) {
   throw new Error("SUPABASE_URL no está definido en .env");
 }
 if (!config.supabaseServiceKey) {
   throw new Error("SUPABASE_SERVICE_KEY no está definido en .env");
+}
+
+// (MODIFICADO) Validación de GCP
+if (!config.gcpProjectId) {
+  throw new Error("GCP_PROJECT_ID no está definido en .env");
+}
+if (!config.gcpLocation) {
+  throw new Error("GCP_LOCATION no está definido en .env");
 }
